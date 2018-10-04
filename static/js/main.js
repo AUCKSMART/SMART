dac1ddfb7279c1229ca07187b855599bfa3c2245
AUCKSMART <lauwoauckland@yahoo.com>
2018-10-05 01:35:49 +0300

    fourth

---
+++ static/js/main.js
@@ -0,0 +1,17 @@
+/*$(document).ready(function(){
+    var from_date = $('#from_date').val();
+    var to_date = $('#to_date').val();
+    $.post('/reports',{from_date:from_date,to_date:to_date},function(data){
+		console.log(data);
+		$('.operation_div').text(data);
+	});
+});
+
+$(document).ready(function(){
+    var from_date = $('#from_date').val();
+    var to_date = $('#to_date').val();
+    $.post('/reports',{from_date:from_date,to_date:to_date},function(data){
+		console.log(data);
+		$('.type_div').text(data);
+	});
+});*/
